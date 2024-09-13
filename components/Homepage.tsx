354d5edbc35b4c60d0bcf9e46acdab6e50e8fcaf
ratanz <ratanrathod870@gmail.com>
2024-09-13 11:06:29 +0530

    more code

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import styles from '@/styles/Homepage.module.css'
-import { FaReact, FaGitAlt, FaGithub, FaNpm, FaNodeJs } from 'react-icons/fa';
+import { FaReact, FaGitAlt, FaGithub, FaNpm, FaNodeJs, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
 import { SiNextdotjs, SiTailwindcss, SiExpress, SiJavascript, SiTypescript, SiMongodb } from 'react-icons/si';
 
 gsap.registerPlugin(ScrollTrigger)
@@ -16,6 +16,8 @@ const Homepage = () => {
   const descriptionRef = useRef<HTMLParagraphElement>(null)
   const techStackTitleRef = useRef<HTMLHeadingElement>(null)
   const iconsRef = useRef<HTMLDivElement>(null)
+  const connectTitleRef = useRef<HTMLParagraphElement>(null)
+  const connectIconsRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const tl = gsap.timeline()
@@ -26,24 +28,24 @@ const Homepage = () => {
       {
         y: 0,
         opacity: 1,
-        duration: 0.7,
-        stagger: 0.2,
-        ease: 'elastic.out(1, 0.8)',
+        duration: 0.4,
+        ease: 'power3.out',
       }
     )
 
     // sub title and description animation
     tl.fromTo([subTitleRef.current, descriptionRef.current, techStackTitleRef.current, iconsRef.current],
       { y: 50, opacity: 0 },
-      { y: 0, opacity: 1, duration: 1.7, stagger: 0.4, ease: 'power3.out' },
-      '-=0.7'
+      { y: 0, opacity: 1, duration: 1.7, stagger: 0.2, ease: 'power3.out' },
+      '-=0.9'
     )
 
     // Helper function to animate gradient text
     const animateGradientText = (element: HTMLElement) => {
       tl.fromTo(element,
         { backgroundSize: '0% 100%', opacity: 0 },
-        { backgroundSize: '100% 100%', opacity: 1, duration: 1.8, stagger: 0.7, ease: 'power2.out' }
+        { backgroundSize: '100% 100%', opacity: 1, duration: 1.8, scrub: 0.8, stagger: 0.2, ease: 'power2.out' },
+        '-=0.8'
       )
     }
 
@@ -51,6 +53,21 @@ const Homepage = () => {
     animateGradientText(subTitleRef.current!)
     animateGradientText(descriptionRef.current!)
     animateGradientText(techStackTitleRef.current!)
+    animateGradientText(connectTitleRef.current!)
+    animateGradientText(connectIconsRef.current!)
+
+    // tl.fromTo(connectTitleRef.current,
+    //   { y: 0, opacity: 0 },
+    //   { y: 0, opacity: 1, duration: 1.7, ease: 'power3.out' },
+    //   '-=0.9'
+    // )
+
+    // tl.fromTo(connectIconsRef.current,
+    //   { y: 0, opacity: 0 },
+    //   { y: 0, opacity: 1, duration: 1.7, ease: 'power3.out' },
+    //   '-=0.9'
+    // )
+
 
     // tech stack title animation
     tl.to(techStackTitleRef.current,
@@ -70,8 +87,8 @@ const Homepage = () => {
       {
         opacity: 1,
         y: 0,
-        duration: 0.7,
-        stagger: 0.3,
+        duration: 0.4,
+        stagger: 0.2,
         ease: 'back.out(1.4)'
       },
       '-=0.75'  // This makes the icons animation start earlier
@@ -111,8 +128,25 @@ const Homepage = () => {
       y: 40, 
       opacity: 0,
       duration: 3,
+      scrub: 2,
       ease: "power3.out",
     }, "-=2.5")
+
+    
+    // scrollTl.to(connectTitleRef.current, { 
+    //   y: 60, 
+    //   opacity: 0, 
+    //   duration: 2.5,
+    //   ease: "power3.inOut"
+    // }, "-=2.5")
+
+    // scrollTl.to(connectIconsRef.current, { 
+    //   y: 60, 
+    //   opacity: 0,
+    //   duration: 2.5,
+    //   ease: "power3.inOut"
+    // }, "-=2.5")
+
 
     scrollTl.to(techStackTitleRef.current, { 
       y: -60, 
@@ -141,26 +175,43 @@ const Homepage = () => {
     <div className='content min-h-screen p-14 font-pretendard w-full bg-gradient-to-t from-zinc-400 to-zinc-200'>
       <div className='flex flex-col items-start justify-center mt-24'>
 
-        <h1 ref={mainTitleRef} className='text-7xl font-bold self-center bg-gradient-to-t from-zinc-500 to-zinc-700 bg-clip-text text-transparent p-6'>
-          Turning Caffeine into Code.
+        <h1 ref={mainTitleRef} className='text-[5.2vw] font-bold self-center bg-gradient-to-t  from-zinc-500 to-zinc-700 bg-clip-text text-transparent p-6'>
+          Turning Caffeine Into Code.
         </h1>
 
         <h2 ref={subTitleRef} className={`text-4xl font-semibold self-center bg-gradient-to-r from-zinc-500 to-zinc-700 bg-clip-text text-transparent tracking-wider p-2 mt-10 `}>
           Hey, I'm Ratan Rathod
         </h2>
 
-        <p ref={descriptionRef} className='text-sm font-medium text-center max-w-2xl mx-auto bg-gradient-to-r from-zinc-500 to-zinc-700 bg-clip-text text-transparent tracking-wide leading-relaxed'>
+        <p ref={descriptionRef} className='text-sm mt-2  font-medium text-center max-w-2xl mx-auto bg-gradient-to-r from-zinc-500 to-zinc-700 bg-clip-text text-transparent tracking-wide leading-relaxed'>
           I'm a frontend developer based in India,
           dedicated to building scalable websites and applications that make a meaningful impact.
           With a focus on user experience, design aesthetics, and developer satisfaction,
           I create interfaces and experiences that captivate users and align with brand identities.
         </p>
+
+        {/* <div className="connect flex flex-col items-center justify-center mt-6 w-full">
+          <p ref={connectTitleRef} className='text-xl font-semibold bg-gradient-to-r from-zinc-500 to-zinc-700 bg-clip-text text-transparent tracking-wider'>Lets connect</p>
+          <div ref={connectIconsRef} className="flex space-x-4 mt-2">
+            <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="text-zinc-700 hover:text-zinc-900 transition-colors duration-300">
+              <FaGithub className="text-2xl" />
+            </a>
+            <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" className="text-zinc-700 hover:text-zinc-900 transition-colors duration-300">
+              <FaLinkedin className="text-2xl" />
+            </a>
+            <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer" className="text-zinc-700 hover:text-zinc-900 transition-colors duration-300">
+              <FaTwitter className="text-2xl" />
+            </a>
+          </div>
+        </div> */}
       </div>
 
-      <div className='flex flex-col items-center justify-center mt-28'>
+
+      <div className='flex flex-col items-center justify-center mt-24'>
         <h1 ref={techStackTitleRef} className={`text-xl font-bold self-center tracking-wider p-2 uppercase ${styles.animatedGradientText}`}>
           Cool tech stack i work with
         </h1>
+
         <div ref={iconsRef} className="flex flex-wrap justify-center gap-4 mt-2">
           <FaReact className="text-4xl text-blue-500" title="React" />
           <SiNextdotjs className="text-4xl text-black" title="Next.js" />
