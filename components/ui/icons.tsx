75c05224fde2d42f2de1008b6ecb8fd60d91382d
Ratan <ratanrathod870@gmail.com>
2025-06-14 22:45:44 +0530

    added new icon

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,5 +1,5 @@
 import Magnetic from "./Magnetic";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 // import { MdEmail } from "react-icons/md";
 import { useRef } from "react";
 import { gsap } from "gsap";
@@ -9,7 +9,6 @@ const Icons = () => {
   const iconsRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-
     // icons animation
     gsap.fromTo(
       iconsRef.current,
@@ -60,6 +59,17 @@ const Icons = () => {
           <FaTwitter className="w-6 h-6" />
         </a>
       </Magnetic>
+      <Magnetic>
+        <a
+          href="https://instagram.com/ratanz.codes"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gradient-to-t text-zinc-400 rounded-md transition-colors"
+        >
+          <FaInstagram className="w-6 h-6" />
+        </a>
+      </Magnetic>
+
       {/* <Magnetic>
         <a
           href="mailto:ratanrathod870@gmail.com"
