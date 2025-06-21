8d7c11b6313ce919342db42671e60bc244207c5c
Ratan <ratanrathod870@gmail.com>
2025-06-21 21:44:47 +0530

    moved project data into diff compo

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { projectsContent } from '@/data/projectsData'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -111,106 +112,5 @@ const Projects = () => {
     )
 }
 
-const projectsContent = [
-    {
-        title: "Studio Size",
-        description: "A modern creative studio built with Next.js, featuring dynamic product management, responsive design, and seamless user experience",
-        imageUrl: "/images/studio.png",
-        projectUrl: "https://studio-size-seven.vercel.app/"
-    },
-    {
-        title: "Brainwave",
-        description: "An AI-powered web application with intuitive interface design, real-time interactions, and advanced animation implementations",
-        imageUrl: "/images/brainwave.png",
-        projectUrl: "https://brainwave-ebon-xi.vercel.app/"
-    },
-    {
-        title: "Dribble Landing Page",
-        description: "A pixel-perfect recreation of Dribbble's landing page, showcasing attention to detail and modern CSS techniques",
-        imageUrl: "/images/dribble.png",
-        projectUrl: "https://dribble-clone-frontend.vercel.app/"
-    },
-    {
-        title: "Rejoice",
-        description: "A dynamic landing page featuring smooth animations, responsive layouts, and optimized performance metrics",
-        imageUrl: "/images/rejoice.png",
-        projectUrl: "https://rejoicex.netlify.app/"
-    },
-    {
-        title: "Glass Glam",
-        description: "An elegant e-commerce interface with glassmorphism design, built using modern web technologies and animations",
-        imageUrl: "/images/glass.png",
-        projectUrl: "#projects"
-    },
-    {
-        title: "sundown",
-        description: "An immersive web experience featuring parallax scrolling effects and interactive UI elements",
-        imageUrl: "/images/sun.png",
-        projectUrl: ""
-    },
-    {
-        title: "Cinematic Odesessy",
-        description: "A cinematic website with dynamic content loading, and engaging user interactions, smooth transitions with each click on the page",
-        imageUrl: "/images/cinematic.png",
-        projectUrl: "https://cinematic-odyssey.vercel.app/"
-    },
-    {
-        title: "Genesis",
-        description: "A sophisticated web application showcasing advanced scroll animations and interactive user experiences",
-        imageUrl: "/images/genesis.png",
-        projectUrl: "https://genesis-omega.vercel.app/"
-    },
-    {
-        title: "artistry",
-        description: "An artistic portfolio platform featuring creative animations, smooth transitions, and responsive gallery layouts",
-        imageUrl: "/images/artistry.png",
-        projectUrl: "https://artistry-omega.vercel.app/"
-    },
-    {
-        title: "serene-drift",
-        description:"A serene and calming website with smooth animations and interactive UI elements",
-        imageUrl: "/images/serenedrift.png",
-        projectUrl: "https://serenedrift.netlify.app/"
-    },
-    {
-        title: "stories supercharged",
-        description:" Leveraging JavaScript & GSAP, I supercharged Instagram Stories with smooth transitions, animations, and dynamic storytelling. From image scaling to interactive highlights, this setup transforms user experiences into something visually stunning.",
-        imageUrl: "/images/stories.png",
-        projectUrl: "https://stories-supercharged.netlify.app/"
-    },
-    {
-        title: "Gen Scroll",
-        description:"A sophisticated web application showcasing advanced scroll animations and interactive user experiences",
-        imageUrl: "/images/genscroll.png",
-        projectUrl: "https://gen-scroll.netlify.app/"
-    },
-    {
-        title: "Serenity Slide",
-        description:"A smooth ride to serenity-slides with smooth animations and interactive UI elements",
-        imageUrl: "/images/serenity.png",
-        projectUrl: "https://serenity-slides.netlify.app/"
-    },
-    {
-        title: "Flux & Flow",
-        description:"Flux & Flow - a cutting-edge design agency platform that pushes the boundaries of web aesthetics and performance.",
-        imageUrl: "/images/flux.png",
-        projectUrl: "https://flux-flow.vercel.app/"
-    },
-    {
-        title: "Zentry - Awwwards Recreation",
-        description:"A pixel-perfect recreation of Awwwards winning gaming agency website, showcasing attention to detail at its finest",
-        imageUrl: "/images/zentry.png",
-        projectUrl: "https://zentryy.vercel.app/"
-    },
-    {
-        title: "Pheonix fits",
-        description:"Not your average e-commerce platform, Pheonix fits is a modern take on online shopping with a focus on user experience and performance",
-        imageUrl: "/images/phoenix.png",
-        projectUrl: "https://phoenix-fits.vercel.app/"
-    }
-    
-
-
-]
 
 export default Projects
