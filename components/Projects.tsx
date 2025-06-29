4f8cc88763665483d648b4ba39798fcd1ce7dc72
Ratan <ratanrathod870@gmail.com>
2025-06-29 21:36:27 +0530

    project sec mobile fixes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -76,9 +76,9 @@ const Projects = () => {
                     ref={(el: HTMLDivElement | null) => { projectRefs.current[index] = el }}
                     className={`card ${index === projectsContent.length - 1 ? 'scroll' : 'pinned'} h-screen w-screen`}
                 >
-                    <div className="project absolute top-[40vh] md:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[95%] md:w-[90%] h-[70%] md:h-[68%] flex flex-col-reverse md:flex-row lg:mt-0 mt-0">
+                    <div className="project absolute top-[50vh] md:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[95%] md:w-[90%] h-[90%] md:h-[68%] flex flex-col-reverse md:flex-row lg:mt-0 mt-0 px-2">
                       
-                        <div className="project-info relative h-[40%] md:h-full md:flex-[1.75] rounded-xl p-4 pt-6 md:p-8 bg-neutral-900">
+                        <div className="project-info relative h-[30%] md:h-full md:flex-[1.75] rounded-xl p-4 pt-6 md:p-8 bg-neutral-900">
                             <div className="project-header flex justify-between items-start">
                                 <h3 className="text-2xl md:text-4xl uppercase font-bold text-zinc-100">{project.title}</h3>
                                 <p className="text-sm md:text-base text-zinc-400">0{index + 1} - 0{projectsContent.length}</p>
@@ -91,7 +91,7 @@ const Projects = () => {
                             </div>
                         </div>
 
-                        <div className="project-img md:flex-[4] rounded-xl overflow-hidden mt-2 md:mt-0 md:ml-4 h-[25vh] md:h-full">
+                        <div className="project-img md:flex-[4] rounded-xl overflow-hidden mt-2 md:mt-0 md:ml-4 h-[70%] md:h-full">
                             <div className="relative w-full h-full">
                                 <Image
                                     src={project.imageUrl}
